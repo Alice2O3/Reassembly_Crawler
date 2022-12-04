b846a32abc526c01ac49ca65df2aea5511fc255d
Alice2O3 <100117897+Alice2O3@users.noreply.github.com>
2022-12-05 01:02:24 +0800

    Update random_agents.bat

--- random_agents.bat
+++ random_agents.bat
@@ -1,5 +1,5 @@
 @echo off
-CALL conda.bat activate python38
+CALL conda.bat activate reassembly_crawler
 CHCP 65001
 python reassembly_crawler.py --help
 python reassembly_crawler.py --input_dir=All_Agents_Grouped --output_dir=Random_Agents --post_processing --download_num=256

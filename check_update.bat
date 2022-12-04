3199e3648ac57aad912d8a336b384392b92a53db
Alice2O3 <100117897+Alice2O3@users.noreply.github.com>
2022-12-05 01:02:04 +0800

    Update check_update.bat

--- check_update.bat
+++ check_update.bat
@@ -1,5 +1,5 @@
 @echo off
-CALL conda.bat activate python38
+CALL conda.bat activate reassembly_crawler
 CHCP 65001
 python reassembly_crawler.py --help
 python reassembly_crawler.py --input_dir=Input --output_dir=All_Agents --crawler_mode=GetLinks --check_update

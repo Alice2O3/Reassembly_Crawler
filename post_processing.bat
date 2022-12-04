82364e91a08c979c149a812527da627a10c0f42d
Alice2O3 <100117897+Alice2O3@users.noreply.github.com>
2022-12-05 00:36:01 +0800

    Delete post_processing.bat

--- post_processing.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-CALL conda.bat activate python38
-CHCP 65001
-python reassembly_crawler.py --help
-python reassembly_crawler.py --input_dir=All_Agents_Grouped --output_dir=All_Agents --post_processing
-@echo on
-pause

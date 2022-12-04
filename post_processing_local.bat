b01b3b0cd03931dd44db7e837d840f9e77975292
Alice2O3 <100117897+Alice2O3@users.noreply.github.com>
2022-12-05 00:33:55 +0800

    Delete post_processing_local.bat

--- post_processing_local.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-CALL conda.bat activate python38
-CHCP 65001
-python reassembly_crawler.py --help
-python reassembly_crawler.py --input_dir=All_Agents_Grouped --output_dir="C:\Users\hydz2\Saved Games\Reassembly\data\agents" --post_processing
-@echo on
-pause
